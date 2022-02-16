d23f136d446e693f836a4ec9c3ba51dafccaee8e
Faisal Sayed <sayedfaisal544@gmail.com>
2022-02-16 22:30:36 +0530

    Star Pattern

---
+++ star_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, row, col;
+    cout << "Enter the number of Rows : ";
+    cin >> row;
+    cout << "Enter the number of Cols : ";
+    cin >> col;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
