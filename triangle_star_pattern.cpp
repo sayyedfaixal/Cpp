dee7a88b63e4369960e1b5fd41c4d79741815e94
Faisal Sayed <sayedfaisal544@gmail.com>
2022-02-16 22:49:50 +0530

    Triagnle Star Pattern

---
+++ triangle_star_pattern.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col;
+    cout << "Enter the number of Rows  : ";
+    cin >> row;
+    cout << "Enter the number of Cols  : ";
+    cin >> col;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
